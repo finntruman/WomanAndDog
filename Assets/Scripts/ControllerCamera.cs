3dc7c980936ad143bf679a19ae0ac1b576271e7c
Unknown <finnttruman@gmail.com>
2017-11-16 11:12:35 +0000

    Project upload
    Uploaded the initial project

---
+++ Assets/Scripts/ControllerCamera.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerCamera : MonoBehaviour {
+
+    public GameObject m_player;
+
+    public float m_xPos;
+    public float m_yPos;
+    public float m_zPos;
+
+    public float m_xDifference;
+    public float m_moveThresh = 2.0f;
+    public float speed = 4.95f;
+
+    void Start()
+    {
+        m_xPos = 0.0f;
+        m_yPos = 4.7f;
+        m_zPos = -10.0f;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        Vector3 target = new Vector3(m_player.transform.position.x, m_yPos, m_zPos);
+
+        // Camera dead zone
+        m_xDifference = m_player.transform.position.x - transform.position.x; // Check what the x difference is betwene player and camera
+        if (Mathf.Abs(m_xDifference) >= m_moveThresh) // if player is too far to either side...
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime); // follow them
+        }
+    }
+}
---
+++ Assets/Scripts/ControllerPlayer.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerPlayer : MonoBehaviour {
+
+    ////////////////////////////
+    /// PRIVATE VARIABLES
+    ////////////////////////////
+
+    // COMPONENTS
+    private Rigidbody   com_rigidbody;
+
+    // CONTROLS
+    public int m_controlsUsed;
+    // KEY CODES
+    private KeyCode key_left;
+    private KeyCode key_right;
+    private KeyCode key_up;
+    private KeyCode key_down;
+    private KeyCode key_jump;
+
+    // GAMEPAD
+    private float m_deadZone;
+
+    ////////////////////////////
+    /// PUBLIC VARIABLES
+    ////////////////////////////
+    //public Camera com_camera;
+    public float m_speed;
+    public float m_jumpForce;
+    public float rotatespeed = 0.1f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        com_rigidbody   = GetComponent<Rigidbody>();
+
+        key_left    = KeyCode.A;
+        key_right   = KeyCode.D;
+        key_up      = KeyCode.W;
+        key_down    = KeyCode.S;
+        key_jump    = KeyCode.Space;
+
+        m_deadZone  = 0.25f;
+
+        m_speed     = 5.0f;
+        m_jumpForce = 400.0f;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float xMovement = 0.0f;
+        float zMovement = 0.0f;
+
+        if (Input.GetKey(key_left))
+        {
+            xMovement -= 1;
+        }
+        if (Input.GetKey(key_right))
+        {
+            xMovement += 1;
+        }
+        if (Input.GetKey(key_down))
+        {
+            zMovement -= 1;
+        }
+        if (Input.GetKey(key_up))
+        {
+            zMovement += 1;
+        }
+
+        // Stick input
+        xMovement = Input.GetAxisRaw("Horizontal");
+        zMovement = Input.GetAxisRaw("Vertical");
+        if (Mathf.Abs(xMovement) < m_deadZone) xMovement = 0.0f;
+        if (Mathf.Abs(zMovement) < m_deadZone) zMovement = 0.0f;
+
+        // Jump
+        if (Input.GetKeyDown(key_jump) /*|| Input.GetButtonDown(btn_jump)*/) com_rigidbody.AddForce(0, m_jumpForce, 0);
+
+        // Code helped created from this forum post: https://answers.unity.com/questions/803365/make-the-player-face-his-movement-direction.html
+
+        Vector3 movement = new Vector3(xMovement, 0.0f, zMovement);
+
+        // If the movement vector is zero, set rotation to itself, else slerp towards the direction of movement
+        transform.rotation = (movement == Vector3.zero) ? transform.rotation : Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+        transform.Translate(movement * m_speed * Time.deltaTime, Space.World);
+	}
+}
