f1e66c42d383ac7e4ffb609c1de968a61ea60b94
Unknown <finnttruman@gmail.com>
2017-12-06 13:38:48 +0000

    Added behaviour tree to dog

---
+++ Assets/Scripts/Behaviour tree AI/BehaviourAI.cs
@@ -0,0 +1,248 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum State
+{
+    FALSE, RUNNING, TRUE, INACTIVE
+}
+
+class Conditions
+{
+    // This is where the condition functions go that are invoked
+    public static bool hasWhistled(ControllerDog agent)
+    {
+        Debug.Log("Checking for whistle...");
+        //return ControllerPlayer.g_player.m_whistled;
+        return false;
+    }
+
+    public static bool inSphere(ControllerDog agent)
+    {
+        Debug.Log("Checking for sphere...");
+        return false;
+    }
+
+    public static bool interacting(ControllerDog agent)
+    {
+        Debug.Log("Checking for interacting...");
+        return false;
+    }
+
+    public static bool tenMinutes(ControllerDog agent)
+    {
+        Debug.Log("Checking minutes...");
+        return false;
+    }
+}
+
+class Actions
+{
+    public static State ApproachPlayer(ControllerDog agent)
+    {
+        Debug.Log("Approaching...");
+        return State.FALSE;
+    }
+
+    public static State Sit(ControllerDog agent)
+    {
+        Debug.Log("Sitting...");
+        return State.FALSE;
+    }
+
+    public static State Sniff(ControllerDog agent)
+    {
+        Debug.Log("Sniffing...");
+        return State.FALSE;
+    }
+
+    public static State RunFast(ControllerDog agent)
+    {
+        Debug.Log("Running fast...");
+        return State.FALSE;
+    }
+
+    public static State Wait(ControllerDog agent)
+    {
+        Debug.Log("Waiting...");
+        return State.FALSE;
+    }
+
+    public static State RunWith(ControllerDog agent)
+    {
+        Debug.Log("Running with...");
+        return State.FALSE;
+    }
+
+    public static State Dissapear(ControllerDog agent)
+    {
+        Debug.Log("Dissapearing...");
+        return State.FALSE;
+    }
+
+    public static State Hunted(ControllerDog agent)
+    {
+        Debug.Log("Hunting...");
+        return State.FALSE;
+    }
+}
+
+public class Node : MonoBehaviour
+{
+    static public ControllerPlayer m_player;
+    public ControllerDog m_agent;
+    public List<Node> m_children;
+    protected State m_state;
+
+    public State GetState() { return m_state; }
+
+    //public virtual State Execute()
+    //{
+    //    return State.FALSE;
+    //}
+
+    public virtual IEnumerator Execute()
+    {
+        m_state = State.FALSE;
+        yield return null;
+    }
+
+    // Adds node to end (right) of list
+    public void AddNode(Node node)
+    {
+        m_children.Add(node);
+    }
+}
+
+public delegate bool Condition(ControllerDog agent);
+public class ConditionNode : Node
+{
+    Condition m_condition;
+
+    public ConditionNode(Condition condition, ControllerDog agent)
+    {
+        m_condition = condition;
+        m_agent = agent;
+    }
+
+    public override IEnumerator Execute()
+    //public override State Execute()
+    {
+        bool success = m_condition.Invoke(m_agent);
+        if (success)
+        {
+            m_state = State.TRUE;
+            //return State.TRUE;
+            yield return null;
+        }
+        else
+        {
+            m_state = State.FALSE;
+            //return State.FALSE;
+            yield return null;
+        }
+    }
+}
+
+public class CompositeNode : Node
+{
+  
+}
+
+// Processes its child nodes in left to right order
+// Suceeds only if ALL child nodes suceed
+// If ANY child node fails, entire sequence fails
+public class SequenceNode : CompositeNode
+{
+    public override IEnumerator Execute()
+    //public override State Execute()
+    {
+        m_state = State.RUNNING;
+        for (int i = 0; i < m_children.Count; i++)
+        {
+            if (m_children[i].GetState() != State.RUNNING)
+            {
+                yield return m_children[i].Execute();
+                if (m_children[i].GetState() == State.FALSE)
+                //if (m_children[i].Execute() == State.FALSE)
+                {
+                    m_state = State.FALSE;
+                    //return m_state;
+                    yield return null;
+                }
+            }
+        }
+        m_state = State.TRUE;
+        yield return null;
+    }
+}
+
+// Tries all of its child nodes in left to right order
+// Succeeds if ANY child node suceeds
+// Fails if ALL child nodes fail
+public class SelectorNode : CompositeNode
+{
+    public override IEnumerator Execute()
+    //public override State Execute()
+    {
+        m_state = State.RUNNING;
+        for (int i = 0; i < m_children.Count; i++)
+        {
+            yield return m_children[i].Execute();
+            if (m_children[i].GetState() == State.TRUE)
+            {
+                m_state = State.TRUE;
+                yield return null;
+            }
+            //if (m_children[i].GetState() != State.RUNNING)
+            //{
+            //    if (m_children[i].Execute() == State.TRUE)
+            //    {
+            //        m_state = State.TRUE;
+            //        return m_state;
+            //    }
+            //}
+        }
+        m_state = State.FALSE;
+        //return m_state;
+        yield return null;
+    }
+}
+
+// Tries a random child node and succeeds if the random node does
+public class StochasticNode : CompositeNode
+{
+    public override IEnumerator Execute()
+    //public override State Execute()
+    {
+        int random = Random.Range(0, m_children.Count - 1);
+        yield return m_children[random].Execute();
+        m_state = (m_children[random].GetState());
+        yield return null;
+    }
+}
+
+public delegate State Action(ControllerDog agent);
+public class ActionNode : Node
+{
+    Action m_action;
+
+    public ActionNode(Action action, ControllerDog agent)
+    {
+        m_action = action;
+        m_agent = agent;
+    }
+
+    public override IEnumerator Execute()
+    //public override State Execute()
+    {
+        m_state = State.RUNNING;
+        yield return m_action.Invoke(m_agent);
+        //if (m_state != State.RUNNING)
+        //{
+        //    m_state = State.RUNNING;
+        //    m_action.Invoke(m_agent);
+        //}
+        //return m_state;
+    }
+}
---
+++ Assets/Scripts/ControllerDog.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerDog : MonoBehaviour {
+
+    SelectorNode m_AI;
+
+	// Use this for initialization
+	void Start ()
+    {
+        // -- ROOT AND ITS CHILDREN --
+        // Root node selector
+        m_AI = new SelectorNode();
+
+        // 1st node of Root
+        Condition whistleCondition = new Condition(Conditions.hasWhistled);
+        ConditionNode whistle = new ConditionNode(whistleCondition, this);
+
+        // 2nd node of Root
+        Condition inSphereCondition = new Condition(Conditions.inSphere);
+        ConditionNode inSphere = new ConditionNode(inSphereCondition, this);
+
+        // 3rd node of Root
+        StochasticNode runFaster = new StochasticNode();
+
+        // Adding children to Root
+        m_AI.AddNode(whistle);
+        m_AI.AddNode(inSphere);
+        m_AI.AddNode(runFaster);
+
+        // -- CHILDREN OF WHISTLE CONDITION (1ST CHILD OF ROOT) --
+        Action approachAction = new Action(Actions.ApproachPlayer);
+        ActionNode approach = new ActionNode(approachAction, this);
+        whistle.AddNode(approach);
+
+        // -- CHILDREN OF IN-SPHERE CONDITION (2ND CHILD OF ROOT) --
+        Condition interactingCondition = new Condition(Conditions.interacting);
+        ConditionNode interacting = new ConditionNode(interactingCondition, this);
+        inSphere.AddNode(interacting);
+
+        // -- CHILDREN OF RUN-FASTER CONDITION (3RD CHILD OF ROOT) --
+        SequenceNode runAhead = new SequenceNode();
+        SequenceNode runWith = new SequenceNode();
+        runFaster.AddNode(runAhead);
+        runFaster.AddNode(runWith);
+
+        // -- CHILDREN OF INTERACTING CONDITION NODE (CHILD OF IN-SPHERE CONDITION) --
+        SequenceNode sitThenHunt = new SequenceNode();
+        SequenceNode idle = new SequenceNode();
+        interacting.AddNode(sitThenHunt);
+        interacting.AddNode(idle);
+
+        // -- CHILDREN OF SIT-THEN-HUNT SEQUENCE (CHILD OF SIT-THEN-HUNT SEQUENCE) --
+        Action sitAction = new Action(Actions.Sit);
+        ActionNode sit = new ActionNode(sitAction, this);
+        SequenceNode huntSequence = new SequenceNode();
+        sitThenHunt.AddNode(sit);
+        sitThenHunt.AddNode(huntSequence);
+
+        // -- CHILDREN OF IDLE SEQUENCE (CHILD OF IDLE SEQUENCE) --
+        Action sniffAction = new Action(Actions.Sniff);
+        ActionNode randomSpot = new ActionNode(sniffAction, this);
+        Condition tenMinutesCondition = new Condition(Conditions.tenMinutes);
+        ConditionNode tenMinutes = new ConditionNode(tenMinutesCondition, this);
+        idle.AddNode(randomSpot);
+        idle.AddNode(tenMinutes); // joint child
+
+        // -- CHILDREN OF RUN-AHEAD SEQUENCE (CHILD OF RUN-FASTER CONDITION) --
+        Action runFastAction = new Action(Actions.RunFast);
+        ActionNode runFast = new ActionNode(runFastAction, this);
+        Action waitAction = new Action(Actions.Wait);
+        ActionNode turnAndWait = new ActionNode(waitAction, this);
+        runAhead.AddNode(runFast);
+        runAhead.AddNode(tenMinutes); // joint child
+        tenMinutes.AddNode(huntSequence);
+        runAhead.AddNode(turnAndWait);
+
+        // -- CHILDREN OF RUN-WITH SEQUENCE (CHILD OF RUN-FASTER CONDITION) --
+        Action runWithAction = new Action(Actions.RunWith);
+        ActionNode runNear = new ActionNode(runWithAction, this);
+        runWith.AddNode(runNear);
+
+        // -- CHILDREN OF HUNT SEQUENCE (CHILD OF SIT-THEN-HUNT SEQUENCE AND TEN-MINUTES CONDITION) --
+        Action dissapearAction = new Action(Actions.Dissapear);
+        ActionNode dissapear = new ActionNode(dissapearAction, this);
+        Action huntedAction = new Action(Actions.Hunted);
+        ActionNode hunted = new ActionNode(huntedAction, this);
+        huntSequence.AddNode(dissapear);
+        huntSequence.AddNode(hunted);
+
+        // START
+        m_AI.StartCoroutine(m_AI.Execute());
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
